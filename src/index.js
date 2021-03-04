c05b7b61b7ddc0ecf7338ed2fac7c63181dbc046
Mehmet Ali Peker <maps6134@hotmail.com>
2021-03-04 03:21:48 +0300

    apollo graphql implemented

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
